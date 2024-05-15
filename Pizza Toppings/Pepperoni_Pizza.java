f66c679fc7307e1f878d67271d453d8bbdabed55
sandwipshanto <moloyswe20@gmail.com>
2024-05-15 23:52:06 +0600

    first commit

---
+++ Pizza Toppings/Pepperoni_Pizza.java
@@ -0,0 +1,7 @@
+package Pizza_Toppings;
+public class Pepperoni_Pizza extends Pizza {
+    @Override
+    public void addTopping(String topping) {
+        System.out.println("Adding " + topping + " to Pepperoni pizza");
+    }
+}
